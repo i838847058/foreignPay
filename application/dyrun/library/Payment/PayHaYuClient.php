c06962bc40e2b291594532313c483af2396c180c
z.shx <winnie_xian@163.com>
2023-11-30 17:05:27 +0800

    请求上游支付平台的支付网关 PayHaYuClient.php +

---
+++ application/dyrun/library/Payment/PayHaYuClient.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace app\dyrun\library\Payment;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * @property ResponseInterface $clientResponse
+ * @property array $jsonRaw
+ */
+trait PayHaYuClient
+{
+
+    public $httpClient;
+
+    /**
+     * @var ResponseInterface
+     */
+    public $clientResponse;
+
+    public $host = 'https://www.payhayu.com';
+
+    public $method = 'POST';
+
+    /**
+     * @var string[]
+     */
+    public $jsonRaw = [];
+
+    private $merchantNo = '6559218556';
+
+    private $PAY_KEY_IN = 'dVptATLGMtIiDaZ2eGvpkw4L5prRXy0I';
+
+    private $PAY_KEY_OUT = 'laKZBIFGgQMTe0z4WagAoZmpGltqpLkY';
+
+    /**
+     * @param string|null $host
+     */
+    public function __construct(string $host = null)
+    {
+        $this->httpClient = new Client([
+            // Base URI is used with relative requests
+            'base_uri' => $host ?: $this->host,
+            // You can set any number of default request options.
+            'timeout' => 5.0,
+        ]);
+    }
+
+    /**
+     * @param array $data
+     * @return string
+     */
+    private function getSignKey(array $data): string
+    {// 对请求参数进行 ASCII 排序
+        ksort($data);
+        // 拼接加密内容
+        $content = http_build_query($data) . "&signKey=" . $this->PAY_KEY_IN;
+        // MD5 加密
+        return strtoupper(md5($content));
+    }
+
+    /**
+     * @param string $merTradeNo
+     * @param string $amount
+     * @param string $name
+     * @param string $tel
+     * @param string $email
+     * @param string|null $callbackUrl
+     * @param string|null $type
+     * @param string|null $extra
+     * @return bool
+     * @throws GuzzleException
+     */
+    public function paymentAdd(string $merTradeNo, string $amount, string $name, string $tel, string $email, string $callbackUrl = null, string $type = null, string $extra = null): bool
+    {
+        $this->jsonRaw['merTradeNo'] = $merTradeNo;
+        $this->jsonRaw['amount'] = $amount;
+        $this->jsonRaw['name'] = $name;
+        $this->jsonRaw['tel'] = $tel;
+        $this->jsonRaw['email'] = $email;
+        if (!empty($callbackUrl)) {
+            $this->jsonRaw['callbackUrl'] = $callbackUrl;
+        }
+        if (!empty($type)) {
+            $this->jsonRaw['type'] = $type;
+        }
+        if (!empty($extra)) {
+            $this->jsonRaw['extra'] = $extra;
+        }
+        // TODO 创建支付订单
+        return $this->requestHandle('/ext_api/v1/payment/add');
+    }
+
+    /**
+     * @param string $merTradeNo
+     * @return bool
+     * @throws GuzzleException
+     */
+    public function paymentQuery(string $merTradeNo): bool
+    {
+        $this->jsonRaw['merTradeNo'] = $merTradeNo;
+        // TODO 创建支付订单
+        return $this->requestHandle('/ext_api/v1/payment/query');
+    }
+
+    /**
+     * @param string $uri
+     * @return bool
+     * @throws GuzzleException
+     */
+    private function requestHandle(string $uri): bool
+    {
+        $this->jsonRaw['merchantNo'] = $this->merchantNo;
+        $this->jsonRaw['sign'] = $this->getSignKey($this->jsonRaw);
+        $this->clientResponse = $this->httpClient->request($this->method, $uri, [
+            'json' => $this->jsonRaw['sign']
+        ]);
+        return true;
+    }
+}
